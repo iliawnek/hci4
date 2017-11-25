80ec906cd4e76045b171cf2b2ce2ec73e06ce7f3
Ken Li <iliawnek@gmail.com>
2017-11-25 00:41:15 +0000

    Move Firebase initialisation into store

--- src/store/index.js
+++ src/store/index.js
@@ -3,6 +3,15 @@ import reducer from "./reducers";
 import {reactReduxFirebase} from 'react-redux-firebase';
 import firebase from 'firebase';
 
+firebase.initializeApp({
+  apiKey: "AIzaSyBHEZL0d-CiO4KVawNgmTwUr5Gby_9o8uk",
+  authDomain: "hci4-170c2.firebaseapp.com",
+  databaseURL: "https://hci4-170c2.firebaseio.com",
+  projectId: "hci4-170c2",
+  storageBucket: "hci4-170c2.appspot.com",
+  messagingSenderId: "79072240282"
+});
+
 const createStoreWithFirebase = compose(
   reactReduxFirebase(firebase, {
     userProfile: 'users',
@@ -11,7 +20,7 @@ const createStoreWithFirebase = compose(
 
 const store = createStoreWithFirebase(
   reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 );
 
 export default store;
