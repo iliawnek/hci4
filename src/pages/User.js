4b4a327dda0c9cdbf11a1fde5e4bf9af991be4ad
Ken Li <iliawnek@gmail.com>
2017-11-25 00:41:00 +0000

    Update User with react-redux-firebase

--- src/pages/User.js
+++ src/pages/User.js
@@ -2,71 +2,54 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { setAppBarTitle } from "../store/reducers/ui";
 import { withRouter } from 'react-router';
-import { db, auth } from '../Firebase';
 import RaisedButton from 'material-ui/RaisedButton';
+import { firebaseConnect } from 'react-redux-firebase';
+import { compose } from 'redux';
 
 class User extends Component {
-  state = {
-    ownPage: false,
-    userData: null
-  }
-
-  componentWillMount() {
-    if (this.props.userData) {
-      this.props.setAppBarTitle(this.props.userData.displayName);
-    }
-  }
-
   componentWillReceiveProps(nextProps) {
-    // if currently signed in user has been loaded
-    if (nextProps.user && nextProps.userData) {
-      const uidParam = nextProps.match.params.uid;
-      const uidLoggedIn = nextProps.user.uid;
-      // if this is user's own page
-      if (uidParam === uidLoggedIn) {
-        this.setState({
-          ownPage: true,
-          userData: nextProps.userData
-        })
-        this.props.setAppBarTitle(nextProps.userData.displayName);
-      }
-      // if this is someone else's page
-      else {
-        db.ref(`users/${uidParam}`).once('value', snapshot => {
-          this.setState({
-            ownPage: false,
-            userData: snapshot.val()
-          })
-          this.props.setAppBarTitle(snapshot.val().displayName);
-        })
-      }
+    if (!this.props.user && nextProps.user) {
+      this.props.setAppBarTitle(nextProps.user.displayName);
     }
   }
 
   signOut = () => {
-    auth.signOut()
+    this.props.firebase.auth().signOut()
     this.props.history.push('/')
   }
 
   render() {
+    const {currentUid} = this.props;
+    const {uid} = this.props.match.params;
+    const ownPage = currentUid === uid;
+
     const styles = {
       user: {}
     };
 
     return (
       <div style={styles.user}>
-        <RaisedButton
+        {ownPage && <RaisedButton
           label="Sign out"
           onClick={this.signOut}
-        />
+        />}
       </div>
     );
   }
 }
 
-export default connect(state => ({
-  user: state.auth.user,
-  userData: state.auth.userData
-}), {
-  setAppBarTitle
-})(withRouter(User));
+export default compose(
+  connect(state => ({
+    currentUid: state.firebase.auth && state.firebase.auth.uid,
+    user: state.firebase.data.user,
+  }), {
+    setAppBarTitle
+  }),
+  withRouter,
+  firebaseConnect((props) => ([
+    {
+      path: `users/${props.match.params.uid}`,
+      storeAs: 'user',
+    },
+  ])),
+)(User);
