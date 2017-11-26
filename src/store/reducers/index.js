d0f8b339cbdae9d9cb9b59939de19b995135505a
Ken Li <iliawnek@gmail.com>
2017-11-26 14:13:30 +0000

    Remove now-unused reducer modules

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,12 +1,8 @@
 import { combineReducers } from "redux";
 import {firebaseReducer} from 'react-redux-firebase';
 import ui from "./ui";
-import auth from "./auth";
-import date from "./date";
 
 export default combineReducers({
   ui,
-  auth,
-  date,
   firebase: firebaseReducer,
 });
